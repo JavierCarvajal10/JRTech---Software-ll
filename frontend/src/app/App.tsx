095d1002f5b45f2ad61a86c61ed60cea0578ca7d
Anthony <sebastianagva@gmail.com>
2026-04-20 22:13:20 -0500

    agregación del carrito

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,14 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes.tsx";
+import { CartProvider } from "./context/CartContext";
+import { ToastProvider } from "./context/ToastContext";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <CartProvider>
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
+    </CartProvider>
+  );
 }
--- frontend/src/app/routes.tsx
+++ frontend/src/app/routes.tsx
@@ -2,10 +2,11 @@ import { createBrowserRouter } from "react-router";
 import { Root } from "./pages/Root";
 import { Home } from "./pages/Home";
 import { Catalog } from "./pages/Catalog";
-import { Auth } from "./pages/Auth";
+//import { Auth } from "./pages/Auth";
 //import { ProductDetail } from "./pages/ProductDetail";
-import { Importaciones } from "./pages/Importaciones";
-import { BuildPC } from "./pages/BuildPC"
+//import { Importaciones } from "./pages/Importaciones";
+import { BuildPC } from "./pages/BuildPC";
+import { Cart } from "./pages/Cart";
 
 export const router = createBrowserRouter([
   {
@@ -14,10 +15,11 @@ export const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: "catalogo", element: <Catalog /> },
-  //    { path: "producto/:id", element: <ProductDetail /> },
-      { path: "importaciones", element: <Importaciones /> },
-      { path: "login", element: <Auth /> },
+//      { path: "producto/:id", element: <ProductDetail /> },
+ //     { path: "importaciones", element: <Importaciones /> },
+  //    { path: "login", element: <Auth /> },
       { path: "arma-tu-pc", element: <BuildPC /> },
+      { path: "carrito", element: <Cart /> },
     ],
   },
 ]);
